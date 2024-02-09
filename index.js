184f7d294b2cfd5455ebb98855305a117befb834
Michael DiPasquale <michaeldipasquale313@gmail.com>
2024-02-08 19:51:07 -0500

    completed creating and inserting DOM nodes lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+//document.body.append(newHeader);
+
+newHeader.setAttribute('id', "victory");
+
+newHeader.textContent = "Michael is the champion";
